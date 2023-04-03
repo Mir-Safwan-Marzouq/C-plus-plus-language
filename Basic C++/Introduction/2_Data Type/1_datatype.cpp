6cf0a931f2f2ce690e5884501b4796b590921ccc
Mir Safwan Marzouq <64838036+Mir-Safwan-Marzouq@users.noreply.github.com>
2023-04-03 14:00:13 +0600

    Add files via upload

---
+++ Basic C++/Introduction/2_Data Type/1_datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    char b;
+    float c;
+    double d;
+    char e[100];
+    bool f =true; //new // false =0; true =  0 baade joto shob value ache shegula
+    cin>>a;
+    cout<<a<<endl;
+    cin>>e;
+    cout<<e<<endl;
+    //cout<<"Hello World"<<endl;
+    return 0;
+}
