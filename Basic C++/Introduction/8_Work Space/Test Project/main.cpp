994f99b4b044258bafafd2319033a4b6634dc75c
Mir Safwan Marzouq <64838036+Mir-Safwan-Marzouq@users.noreply.github.com>
2023-04-09 00:02:13 +0600

    Add files via upload

---
+++ Basic C++/Introduction/8_Work Space/Test Project/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "include/student.h"
+using namespace std;
+
+
+//oop er concept hocche shobkichu module bhittik kaaj korte chai
+// class create korbo
+
+int main()
+{
+    student obj(123);
+    obj.print();
+    return 0;
+}
